a4bdd826d83dcaa3b9e0405fb0fba2ce59fe02ca
aleks-lazic <aleksandar.lazic.1995@gmail.com>
2020-04-14 16:46:59 +0200

    compiling ok in neocompiler

--- VickreyAuction.cs
+++ VickreyAuction.cs
@@ -1,85 +1,60 @@
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
+using System.Numerics;
 using System;
 
-
-// Key is totalSupply and value is 100000000
-//Storage.Put(Storage.CurrentContext, "totalSupply", 100000000);
-
-//Get the totalSupply in the storageMap. The Map is used an entire container with key name "contract"
-//StorageMap contract = Storage.CurrentContext.CreateMap(nameof(contract));
-//return contract.Get("totalSupply").AsBigInteger();
-
-//Storage.Put(Storage.CurrentContext, domain, owner);
-//byte[] value = Storage.Get(Storage.CurrentContext, domain);
-
-//Storage.Delete
-
-// validate the address invoking
-//if (!Runtime.CheckWitness(owner))
-//return false;
-
-//public static event transfer(byte[] from, byte[] to, BigInteger amount)
-
-
 namespace VickreyAuction {
+    public class VickreyAuction : SmartContract {
+        
+        public static string Main(string method, params object[] args){
+            //asset transfer
+            if(Runtime.Trigger == TriggerType.Application) return "OwnerNotKnown, NEED TO HAVE IT IN PARAMS"; //return Runtime.CheckWitness(Owner);
 
-  public class Contract1 : SmartContract {
-
-
-    public static String Main(string method, params object[] args){
-
-            //asset transfer (user may to send global asset such as NEO or GAS to or from this contract)
-            if (Runtime.Trigger == TriggerType.Verification){
-                return Runtime.CheckWitness(Owner);
-            }
-
-
-            //no asset transfer
+           //no asset transfer
             if (Runtime.Trigger == TriggerType.Application) {
-                    switch (method) {
+                switch (method) {
                     case "Initialization":
-                            return Initialization(args[0], args[1], args[2], args[3]);
+                            return Initialization((byte[])args[0], (int)args[1], (int)args[2],(int)args[3]);
                     case "Bid":
-                            return Bid();
+                            return Bid((string)args[0]);
                     case "Claim":
                             return Claim();
                     case "Transfer":
-                            return Transfer(args[0], args[1], args[2], args[3]);
+                            return Transfer((byte[])args[0], (byte[])args[1], (BigInteger)args[2], (byte[])args[3]);
                     case "Reveal":
-                            return Reveal(args[0], args[1]);
+                            return Reveal((int)args[0], (int)args[1]);
                     case "Withdraw":
                             return Withdraw();
                     default:
-                            return false;
-                    }
+                            return method;
+                }
             }
+            return method;
+        }
+        
+        //VICKREY METHODS
+        //I SET ALL METHODS TO VOID FOR THE MOMENT
+        private static string Initialization(/*TODO : check type*/byte[] token, int reservePrice, int biddingPeriod, int revealingPeriod){
+            //TODO: OWNER MUST BE IN PARAMS
+            //if (!Runtime.CheckWitness(owner)) return false;
+            Storage.Put(Storage.CurrentContext, token, token);
+            
+            // TODO
+            return "";
 
-
-
-            return test_str;
-    }
-
-    private static Initialization(/*type*/ token, int reservePrice, int biddingPeriod, int revealingPeriod){
-      if (!Runtime.CheckWitness(owner)) return false;
-
-      Storage.Put(Storage.CurrentContext, token, token);
-
-      // TODO
-
-    }
-
-    private static Bid(string hash){
-      //TODO
-    }
-
-
-    private static Claim(){
-      //TODO
-    }
-
-
-    private static bool Transfer(byte[] from, byte[] to, BigInteger amount, byte[] callscript){
+        }
+    
+        private static string Bid(string hash){
+            //TODO
+            return "";
+        }
+    
+        private static string Claim(){
+            //TODO
+            return "";
+        }
+    
+        private static string Transfer(byte[] from, byte[] to, BigInteger amount, byte[] callscript){
             //Check parameters
             //transfer the wallet address to byte array
             if (from.Length != 20 || to.Length != 20)
@@ -87,13 +62,13 @@ namespace VickreyAuction {
             if (amount <= 0)
                     throw new InvalidOperationException("The parameter amount MUST be greater than 0.");
             if (!Runtime.CheckWitness(from))
-                    return false;
+                    return "false";
             StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
             var fromAmount = asset.Get(from).AsBigInteger();
             if (fromAmount < amount)
-                    return false;
+                    return "false";
             if (from == to)
-                    return true;
+                    return "true";
 
             //Reduce payer balances
             if (fromAmount == amount)
@@ -105,18 +80,19 @@ namespace VickreyAuction {
             var toAmount = asset.Get(to).AsBigInteger();
             asset.Put(to, toAmount + amount);
 
-            Transferred(from, to, amount);
-            return true;
+            //Transferred(from, to, amount);
+            //return true;
+            return "True";
+        }
+    
+        private static string Reveal(int amount, int nonce){
+            //TODO
+            return "";
+        }
+    
+        private static string Withdraw(){
+            //TODO
+            return "";
+        }
     }
-
-    private static Reveal(int amount, int nonce){
-      //TODO
-    }
-
-    private static Withdraw(){
-      //TODO
-    }
-
-  }
-
 }
