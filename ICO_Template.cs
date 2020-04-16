26e7585191e8ee3d0668a130ba9d737e81bd966f
inefix <inefix3000@hotmail.com>
2020-04-16 17:59:32 +0200

    Deploy

--- ICO_Template.cs
+++ ICO_Template.cs
@@ -19,6 +19,7 @@ namespace Neo.SmartContract
 
         //ICO Settings
         private static readonly byte[] neo_asset_id = { 155, 124, 255, 218, 166, 116, 190, 174, 15, 147, 14, 190, 96, 133, 175, 144, 147, 229, 254, 86, 179, 74, 92, 34, 12, 205, 207, 110, 252, 51, 111, 197 };
+        private static readonly byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
         private const ulong total_amount = 100000000 * factor; // total token amount
         private const ulong pre_ico_cap = 30000000 * factor; // pre ico token amount
         private const ulong basic_rate = 1000 * factor;
